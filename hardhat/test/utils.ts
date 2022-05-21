5be63f0b8822ab80fdf9e53d8c1ebdc36e8928ac
Andrew <fleming-andrew@protonmail.com>
2022-05-21 18:57:21 -0400

    remove unused imports

--- hardhat/test/utils.ts
+++ hardhat/test/utils.ts
@@ -1,5 +1,2 @@
-import { ethers } from "hardhat";
-import { BigNumber } from "ethers";
-
 export const proposalArgs1 = ["Sweet title", "Cowabunga description", "Gnarly location"]
 export const proposalArgs2 = ["Trite Title", "Insipid description", "Loser location"]
